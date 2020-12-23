a64c30945763fbe392679330b800ca2001340193
Mohamed Roshdy <mhmdrshdy29@gmail.com>
2020-12-23 12:40:11 +0200

    HTML Create Project

--- src/app/Components/Projects/create-project/create-project.component.ts
+++ src/app/Components/Projects/create-project/create-project.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-project.component.css']
 })
 export class CreateProjectComponent implements OnInit {
+projectObj:any;
+lstClients:any;
+lstOrganizations:any;
+lstEmployees:any;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.projectObj={id:0,projectName:"",projectCode:"",type:"",cost:0,projectPeriod:0,planndedStartDate:new Date()
+    ,actualStartDate:new Date(),planndedEndDate:new Date(),actualEndDate:new Date(),description:"",organizationId:0 ,employeeId:0,clientId:0}
 
+  }
 }
